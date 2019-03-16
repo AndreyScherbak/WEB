5d6a9941545585597e592b5380c138f4f7837999
AndreyScherbak <t22304@gmail.com>
2019-03-16 19:55:23 +0200

    new project

---
+++ BookStore/BookStore/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+
+namespace BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult BookList()
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(Server.MapPath("~/XML/BookStoreDB.xml"));
+            XmlElement xRoot = xDoc.DocumentElement;
+            XmlNodeList childnodes = xRoot.SelectNodes("*");
+
+            List<Book> books = new List<Book>();
+
+            foreach (XmlNode book in childnodes)
+            {
+                books.Add(new Book { Category = book.ChildNodes[0].InnerText, Title = book.ChildNodes[1].InnerText,
+                    Author = book.ChildNodes[2].InnerText, Price = book.ChildNodes[3].InnerText });
+            }
+            return View(books);
+        }
+    }
+}
---
+++ BookStore/BookStore/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Models
+{
+    public class Book
+    {
+        public string Category { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Price { get; set; }
+    }
+}
